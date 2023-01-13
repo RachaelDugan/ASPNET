07a9ecb4b4eb59dfd83327d650055d6fc5b1fcf2
RachaelDugan <118020235+RachaelDugan@users.noreply.github.com>
2023-01-13 08:56:50 -0700

    Step One complete

---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using Testing.Models;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _connection = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _connection.Query<Product>("Select * From products");
+        }
+    }
+}
